477e36cfbebb3efc00ca5daf83969e24795974a4
dev <dev@360ithub.com>
2025-06-19 09:48:09 +0000

    User modification for Mobile OTP fields

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -110,6 +110,8 @@ class User(Document):
 		logout_all_sessions: DF.Check
 		middle_name: DF.Data | None
 		mobile_no: DF.Data | None
+		mobile_otp: DF.Data | None
+		mobile_otp_generated_at: DF.Datetime | None
 		module_profile: DF.Link | None
 		mute_sounds: DF.Check
 		new_password: DF.Password | None
